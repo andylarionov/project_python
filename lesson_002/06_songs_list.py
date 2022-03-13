091d0a1249b4152498250195db70b44218e40f24
Andy Larionov <andrewlarionov.urtk@gmail.com>
2022-03-13 21:16:18 +0500

    Расчеты выполнены

--- lesson_002/06_songs_list.py
+++ lesson_002/06_songs_list.py
@@ -21,6 +21,8 @@ violator_songs_list = [
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
 # TODO здесь ваш код
+three_songs=violator_songs_list[3][1]+violator_songs_list[5][1]+violator_songs_list[8][1]
+print('Три песни звучат ', round(three_songs,2), 'минут')
 
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
@@ -39,3 +41,5 @@ violator_songs_dict = {
 #   А другие три песни звучат ХХХ минут
 
 # TODO здесь ваш код
+another_three=violator_songs_dict['Sweetest Perfection']+violator_songs_dict['Policy of Truth']+violator_songs_dict['Blue Dress']
+print('А другие три песни звучат ', round(another_three,2), 'минут')
